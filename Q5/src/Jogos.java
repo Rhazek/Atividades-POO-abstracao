4228ac8393438f5d5377d171caa5bd143a6b8f23
Rhazek <matheus_rhazek@hotmail.com>
2024-03-04 11:55:48 -0300

    atualizações

---
+++ Q5/src/Jogos.java
@@ -0,0 +1,18 @@
+public class Jogos {
+
+    public String timeCasa;
+    public String timeVisitante;
+    public int quantIngressosVendidos;
+    public double valorIngresso;
+
+    public Jogos(String timeCasa, String timeVisitante, int quantIngressosVendidos, double valorIngresso) {
+        this.timeCasa = timeCasa;
+        this.timeVisitante = timeVisitante;
+        this.quantIngressosVendidos = quantIngressosVendidos;
+        this.valorIngresso = valorIngresso;
+    }
+
+
+
+
+}
