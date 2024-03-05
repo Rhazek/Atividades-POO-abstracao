c0c04996af06bed1360514a8a4cbfa32f95adc38
Rhazek <matheus_rhazek@hotmail.com>
2024-03-05 09:47:52 -0300

    Resolvendo a Questão 5

--- Q5/src/Jogos.java
+++ Q5/src/Jogos.java
@@ -4,15 +4,31 @@ public class Jogos {
     public String timeVisitante;
     public int quantIngressosVendidos;
     public double valorIngresso;
+    public double faturamento;
+    public double parteTimeCasa;
+    public double parteTimeVisitante;
 
-    public Jogos(String timeCasa, String timeVisitante, int quantIngressosVendidos, double valorIngresso) {
+    public Jogos(String timeCasa, String timeVisitante, int quantIngressosVendidos,
+                 double valorIngresso, double faturamento, double parteTimeCasa, double parteTimeVisitante) {
         this.timeCasa = timeCasa;
         this.timeVisitante = timeVisitante;
         this.quantIngressosVendidos = quantIngressosVendidos;
         this.valorIngresso = valorIngresso;
+        this.faturamento = faturamento;
+        this.parteTimeCasa = parteTimeCasa;
+        this.parteTimeVisitante = parteTimeVisitante;
     }
 
+    public double calculaFaturamento(int quantIngressosVendidos, double valorIngresso){
+        return quantIngressosVendidos * valorIngresso;
+    }
 
+    public double calculaParteTimeCasa(double faturamento){
+        return (faturamento*70)/100;
+    }
 
+    public double calculaParteTimeVisitante (double faturamento){
+        return (faturamento*30)/100;
+    }
 
 }
--- Q5/src/Main.java
+++ Q5/src/Main.java
@@ -7,5 +7,42 @@ public class Main {
         System.out.println("Quantos jogos você quer contabilizar?");
         int numJogos = Integer.parseInt(scan.nextLine());
 
+        Jogos[] jogos = new Jogos[numJogos];
+
+        for (int i = 0; i < numJogos; i++) {
+            System.out.println(String.format("Digite o nome do time mandante do jogo %d", i+1));
+            String timeCasa = scan.nextLine();
+
+            System.out.println(String.format("Digite o nome do time visitante do jogo %d", i+1));
+            String timeVisitante = scan.nextLine();
+
+            System.out.println(String.format("Quantos ingressos foram vendidos no jogo %d?", i+1));
+            int quantIngressosVendidos = Integer.parseInt(scan.nextLine());
+
+            System.out.println(String.format("E quanto custava cada um desses ingressos do jogo %d?", i+1));
+            double valorIngresso = Double.parseDouble(scan.nextLine());
+
+            jogos[i] = new Jogos(timeCasa, timeVisitante, quantIngressosVendidos,
+                    valorIngresso, 0, 0, 0);
+            jogos[i].faturamento = jogos[i].calculaFaturamento(jogos[i].quantIngressosVendidos,
+                    jogos[i].valorIngresso);
+            jogos[i].parteTimeCasa = jogos[i].calculaParteTimeCasa(jogos[i].faturamento);
+            jogos[i].parteTimeVisitante = jogos[i].calculaParteTimeVisitante(jogos[i].faturamento);
+        }
+
+        for (int i = 0; i < numJogos; i++) {
+            System.out.println("""
+                    *********JOGO %d*********
+                    %s  x  %s
+                    Quantidade de ingressos vendidos: %d.
+                    Valor por ingresso: R$%.2f.
+                    Faturamento: R$%.2f
+                    Parte do Time Mandante: R$%.2f
+                    Parte do Time Visitante: R$%.2f
+                    """.formatted(i+1, jogos[i].timeCasa, jogos[i].timeVisitante,
+                    jogos[i].quantIngressosVendidos, jogos[i].valorIngresso, jogos[i].faturamento,
+                    jogos[i].parteTimeCasa, jogos[i].parteTimeVisitante));
+        }
+
     }
 }
